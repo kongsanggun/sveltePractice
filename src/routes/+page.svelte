2a4aa5ff84cfebb7f6b117d6536c77b5885e7e7f
kongsanghyean <dodoi77@naver.com>
2023-09-26 20:16:20 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>test</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+</section>
+
+<style>
+
+</style>
